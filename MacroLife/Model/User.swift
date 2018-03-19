026dabddd33f4e9eff04fd71df06a00ae6fb9c8e
Tyler Clonts <tylerclonts@Tylers-MacBook-Air.local>
2018-03-19 14:42:36 -0600

    Added all my storyboard layout. I still need to add constraints. Added swift files to my model folder and modelcontroller folder.

---
+++ MacroLife/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  MacroLife
+//
+//  Created by Tyler Clonts on 3/19/18.
+//  Copyright © 2018 Tyler Clonts. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    
+    
+}
---
+++ MacroLife/ModelControllers/UsersController.swift
@@ -0,0 +1,9 @@
+//
+//  UsersController.swift
+//  MacroLife
+//
+//  Created by Tyler Clonts on 3/19/18.
+//  Copyright © 2018 Tyler Clonts. All rights reserved.
+//
+
+import Foundation
